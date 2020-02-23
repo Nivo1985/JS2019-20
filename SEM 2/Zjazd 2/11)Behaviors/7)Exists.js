89694972e7d5cd8342390f65f6345eab35459f96
Nivo1985 <karol.rogowski@gmail.com>
2020-02-23 19:52:29 +0100

    Add files via upload

--- SEM 2/Zjazd 2/11)Behaviors/7)Exists.js
+++ SEM 2/Zjazd 2/11)Behaviors/7)Exists.js
@@ -3,17 +3,18 @@ var myObject = {
   c: undefined
 };
 
-// console.log(myObject.a);
-// console.log(myObject.b);
-// console.log(myObject.c);
+console.log(myObject.a); // 2
+console.log(myObject.b); // undefined
+console.log(myObject.c); // undefined
+console.log(myObject.constructor); // function
 
-// // Uses prototype
-// console.log("a" in myObject); // true
-// console.log("b" in myObject); // false
-// console.log("c" in myObject); // true
-// console.log("constructor" in myObject); // true
+// Uses prototype
+console.log("a" in myObject); // true
+console.log("b" in myObject); // false
+console.log("c" in myObject); // true
+console.log("constructor" in myObject); // true
 
-// console.log(myObject.hasOwnProperty("a")); // true
-// console.log(myObject.hasOwnProperty("b")); // false
-// console.log(myObject.hasOwnProperty("c")); // true
-// console.log(myObject.hasOwnProperty("constructor")); // false
+console.log(myObject.hasOwnProperty("a")); // true
+console.log(myObject.hasOwnProperty("b")); // false
+console.log(myObject.hasOwnProperty("c")); // true
+console.log(myObject.hasOwnProperty("constructor")); // false
